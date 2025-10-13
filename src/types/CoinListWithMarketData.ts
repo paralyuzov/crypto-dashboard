579ca25b16d24aeec2987bd598e555c4179ad8b0
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-10-13 20:10:16 +0300

    feat(types): add CoinListWithMarketData interface for CoinGecko market data

--- src/types/CoinListWithMarketData.ts
+++ src/types/CoinListWithMarketData.ts
@@ -1,1 +1,63 @@
+
+export interface CoinListWithMarketData {
+  id: string
+  symbol: string
+  name: string
+  image: string
+  current_price: number
+  market_cap: number
+  market_cap_rank: number
+  fully_diluted_valuation: number | null
+  total_volume: number
+  high_24h: number
+  low_24h: number
+  price_change_24h: number
+  price_change_percentage_24h: number
+  market_cap_change_24h: number
+  market_cap_change_percentage_24h: number
+  circulating_supply: number
+  total_supply: number | null
+  max_supply: number | null
+  ath: number
   ath_change_percentage: number
+  ath_date: string
+  atl: number
+  atl_change_percentage: number
+  atl_date: string
+  roi: {
+    times: number
+    currency: string
+    percentage: number
+  } | null
+  last_updated: string
+}
+
+export const exampleCoin: CoinListWithMarketData = {
+    "id": "bitcoin",
+    "symbol": "btc",
+    "name": "Bitcoin",
+    "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+    "current_price": 70187,
+    "market_cap": 1381651251183,
+    "market_cap_rank": 1,
+    "fully_diluted_valuation": 1474623675796,
+    "total_volume": 20154184933,
+    "high_24h": 70215,
+    "low_24h": 68060,
+    "price_change_24h": 2126.88,
+    "price_change_percentage_24h": 3.12502,
+    "market_cap_change_24h": 44287678051,
+    "market_cap_change_percentage_24h": 3.31157,
+    "circulating_supply": 19675987,
+    "total_supply": 21000000,
+    "max_supply": 21000000,
+    "ath": 73738,
+    "ath_change_percentage": -4.77063,
+    "ath_date": "2024-03-14T07:10:36.635Z",
+    "atl": 67.81,
+    "atl_change_percentage": 103455.83335,
+    "atl_date": "2013-07-06T00:00:00.000Z",
+    "roi": null,
+    "last_updated": "2024-04-07T16:49:31.736Z"
+
+}
