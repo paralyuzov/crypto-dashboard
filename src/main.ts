754a8a13127fcfcd7a23c64f257f43807c45dd06
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-10-13 20:11:34 +0300

    import icons for vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './styles.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { mdi } from 'vuetify/iconsets/mdi'
 
 import App from './App.vue'
 import router from './router'
@@ -12,6 +14,12 @@ import router from './router'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi,
+    },
+  },
 })
 
 const app = createApp(App)
